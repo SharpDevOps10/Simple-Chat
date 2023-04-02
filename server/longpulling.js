3b54150410e8e41917dcae22b07bc754a885aad8
dtimofeev <td090833@gmail.com>
2023-04-02 19:34:22 +0300

    Added basic file

---
+++ server/longpulling.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const cors = require('cors');
+const PORT = 5000;
+const app = express();
+
+app.listen(app.listen(PORT, () => console.log(`server started on port ${PORT}`)));
