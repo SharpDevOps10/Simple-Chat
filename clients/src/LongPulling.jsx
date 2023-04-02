fe6eb798e7f2951584fd4fd46be50e82d39b1c2b
dtimofeev <td090833@gmail.com>
2023-04-02 19:55:19 +0300

    Added message block

--- clients/src/LongPulling.jsx
+++ clients/src/LongPulling.jsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 const LongPulling = () => {
+  const [messages, setMessages] = useState([]);
   return (
     <div className="center">
       <div className="form">
         <input type="text"/>
         <button>Send</button>
+      </div>
+      <div className="messages">
+        {messages.map((mess) => {
+          <div className="message" key={mess.id}>
+            {mess.message}
+
+
+          </div>
+        })};
+
       </div>
 
     </div>
@@ -12,5 +23,5 @@ const LongPulling = () => {
   );
 
 
-};
+}
 export default LongPulling;
