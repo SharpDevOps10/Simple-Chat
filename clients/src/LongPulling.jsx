853b5109d993b15298c2fe8c7d9941d23c88b246
dtimofeev <td090833@gmail.com>
2023-04-02 20:03:56 +0300

    Implemented async function `sendMessages`

--- clients/src/LongPulling.jsx
+++ clients/src/LongPulling.jsx
@@ -1,20 +1,27 @@
 import React, {useState} from "react";
+import axios from "axios";
 const LongPulling = () => {
   const [messages, setMessages] = useState([]);
+  const [value, setValue] = useState('');
+  const sendMessages = async () => {
+    await axios.post('http://localhost:5000', {
+      messages : value,
+      id : Date.now(),
+    })
+
+  };
   return (
     <div className="center">
       <div className="form">
-        <input type="text"/>
-        <button>Send</button>
+        <input value={value} onChange={e => setValue(e.target.value)} type="text"/>
+        <button onClick={setMessages}>Send</button>
       </div>
       <div className="messages">
-        {messages.map((mess) => {
+        {messages.map((mess) =>
           <div className="message" key={mess.id}>
             {mess.message}
-
-
           </div>
-        })};
+        )};
 
       </div>
 
