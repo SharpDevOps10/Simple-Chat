1a3512aae920f2c4243223ed5f8d031d1705a99c
dtimofeev <td090833@gmail.com>
2023-04-07 19:39:44 +0300

    Updated Chat

--- newchat/src/components/Chat.js
+++ newchat/src/components/Chat.js
@@ -1,9 +1,33 @@
-import React from "react";
+import React, {useContext} from "react";
+import {Context} from "../index";
+import {useAuthState} from "react-firebase-hooks/auth";
+import {Button, Container, Grid, TextField} from "@mui/material";
 const Chat = () => {
+  const {auth,firestore} = useContext(Context);
+  const [user] = useAuthState(auth);
   return (
-    <div>
-      Chat
-    </div>
+    <Container>
+      <Grid container
+            style={{height : window.innerHeight - 50, marginTop : 20} }
+
+            justify={"center"}>
+            <div style={{width : '8.0%', height : '70vh', border : '1px solid gray', overflowY : 'auto'}}>
+
+
+            </div>
+        <Grid
+          container
+          direction={"column"}
+          alingItems={"flex-end"}
+          style={{width : '80%'}}
+        >
+          <TextField variant={"outlined"}/>
+          <Button>Send</Button>
+
+        </Grid>
+
+      </Grid>
+    </Container>
 
   );
 }
--- newchat/src/components/Loader.js
+++ newchat/src/components/Loader.js
@@ -22,3 +22,4 @@ const Loader = () => {
   );
 
 };
+export default Loader;
