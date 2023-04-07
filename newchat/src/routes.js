dab63d426749e793d084395b8918cf7f22519fcb
dtimofeev <td090833@gmail.com>
2023-04-07 17:58:04 +0300

    Improved code formatting

--- newchat/src/routes.js
+++ newchat/src/routes.js
@@ -1,17 +1,19 @@
-import {CHAT_ROUTE, LOGIN_ROUTE} from "./utilities/constants";
+import { CHAT_ROUTE, LOGIN_ROUTE } from "./utilities/constants";
 import Login from "./components/Login";
 import Chat from "./components/Chat";
 
 export const publicRoutes = [
   {
-    path : LOGIN_ROUTE,
-    Component : Login,
-  }
+    path: LOGIN_ROUTE,
+    Component: Login,
+  },
 ];
+
 export const privateRoutes = [
   {
-    path : CHAT_ROUTE,
-    Component : Chat,
-  }
+    path: CHAT_ROUTE,
+    Component: Chat,
+  },
 ];
+
 
