719f533085feb6ccb4c5d13bc21a87068cf7321d
dtimofeev <td090833@gmail.com>
2023-04-07 10:44:05 +0300

    Logged in -> button `Exit`

--- newchat/src/components/Navbar.js
+++ newchat/src/components/Navbar.js
@@ -1,14 +1,21 @@
 import React from "react";
 import {AppBar, Button, Grid, Toolbar} from "@mui/material";
+import {NavLink} from "react-router-dom";
+import {LOGIN_ROUTE} from "../utilities/constants";
 const Navbar = () => {
+  const user = true;
   return (
     <div>
       <AppBar color={"secondary"} position="static">
         <Toolbar>
           <Grid container justify={"flex-end"}>
-            <Button variant={"outlined"}>Login</Button>
-            <Button variant={"outlined"}>Exit</Button>
-
+            {user ?
+              <Button variant={"outlined"}>Exit</Button>
+              :
+              <NavLink to={LOGIN_ROUTE}>
+                <Button variant={"outlined"}>Log in</Button>
+              </NavLink>
+            }
           </Grid>
 
         </Toolbar>
