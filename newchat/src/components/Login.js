4e5e01f5edfd9a8b14bd486c26572171bf69482b
dtimofeev <td090833@gmail.com>
2023-04-07 18:53:50 +0300

    Added async function `login`

--- newchat/src/components/Login.js
+++ newchat/src/components/Login.js
@@ -1,6 +1,17 @@
-import React from "react";
+import React, {useContext} from "react";
 import {Box, Button, Container, Grid} from "@mui/material";
+import {Context} from "../index";
+import firebase from 'firebase/compat/app';
+
 const Login = () => {
+  const {auth} = useContext(Context);
+  const login = async () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    const user = await auth.signInWithPopup(provider);
+    console.log(user);
+  };
+
+
   return (
     <Container>
       <Grid container
@@ -14,7 +25,7 @@ const Login = () => {
               direction={"column"}
         >
           <Box p={5}>
-            <Button>Enter with Google</Button>
+            <Button onClick={login} variant={"outlined"}>Enter with Google</Button>
           </Box>
         </Grid>
       </Grid>
