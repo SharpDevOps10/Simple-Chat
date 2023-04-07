c4bc2f9e4dce2bd518671191b111f650325feec1
dtimofeev <td090833@gmail.com>
2023-04-07 18:54:29 +0300

    Got rid of bugs that are connected with deprecation

--- newchat/src/index.js
+++ newchat/src/index.js
@@ -3,41 +3,41 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-import firebase from  "firebase/compat";
-import 'firebase/firestore'
-import 'firebase/auth'
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBQBMG3DvXHBUCy2cWrxFibRTcD5cfdfEY",
-  authDomain: "chat-b34d1.firebaseapp.com",
-  projectId: "chat-b34d1",
-  storageBucket: "chat-b34d1.appspot.com",
-  messagingSenderId: "43591901523",
-  appId: "1:43591901523:web:684c8da13ed92bdd4281a6",
-  measurementId: "G-74RCQFCXJE"
-};
-export const Context = createContext(null);
-const auth = firebase.auth();
-const firestore = firebase.firestore();
+firebase.initializeApp({
+    apiKey: "AIzaSyBr1JWix2dc-VoBCB7fHVSGS9KUL0X0Wis",
+    authDomain: "chat-react-fd366.firebaseapp.com",
+    projectId: "chat-react-fd366",
+    storageBucket: "chat-react-fd366.appspot.com",
+    messagingSenderId: "1097930185910",
+    appId: "1:1097930185910:web:206a1158ff7075b6381a17",
+    measurementId: "G-Q9W58YQ4XL"
+  }
+);
 
+export const Context = createContext(null)
 
+const auth = firebase.auth()
+const firestore = firebase.firestore()
 
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <Context.Provider value={{
     firebase,
     auth,
     firestore
-  }}
-  >
-    <App/>
-
+  }}>
+    <App />
   </Context.Provider>,
-  document.getElementById('root')
 );
+
+
+
 
 
 
