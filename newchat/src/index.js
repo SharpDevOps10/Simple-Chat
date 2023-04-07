574cc5e9de057a7c0406076834bd3cee0d0425ca
dtimofeev <td090833@gmail.com>
2023-04-07 18:27:25 +0300

    Added firebase to index.js

--- newchat/src/index.js
+++ newchat/src/index.js
@@ -1,17 +1,44 @@
-import React from 'react';
+import React, {createContext} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import firebase from  "firebase/compat";
+import 'firebase/firestore'
+import 'firebase/auth'
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBQBMG3DvXHBUCy2cWrxFibRTcD5cfdfEY",
+  authDomain: "chat-b34d1.firebaseapp.com",
+  projectId: "chat-b34d1",
+  storageBucket: "chat-b34d1.appspot.com",
+  messagingSenderId: "43591901523",
+  appId: "1:43591901523:web:684c8da13ed92bdd4281a6",
+  measurementId: "G-74RCQFCXJE"
+};
+export const Context = createContext(null);
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
+
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+ReactDOM.render(
+  <Context.Provider value={{
+    firebase,
+    auth,
+    firestore
+  }}
+  >
+    <App/>
+
+  </Context.Provider>,
+  document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+
